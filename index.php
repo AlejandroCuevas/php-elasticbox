2b56dd896a48fff5652eddf50f493ae190789c81
sandraur9 <sandraur9@gmail.com>
2014-12-10 11:14:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -33,17 +33,23 @@
 					<?php		
 						echo "PRUEBA PHP";
 					 /*mysql.hostinger.es*/
-						$idConn = mysql_connect("$mysql_service.address.public:$mysql_service.port","$mysql_service.username","$mysql_service.password");
-						mysql_select_db('prueba',$idConn);
-						$query = 'SELECT * FROM prueba.estados';
-						$res = mysql_query($query, $idConn) or die(mysql_error());
-						$verbos = array();
-						while($row = mysql_fetch_row($res)){
-							echo '<tr>
-									<td>'.$row[0].'</td>
-									<td>'.$row[1].'</td>
-									<td>'.$row[2].'</td>
-								  </tr>';
+						//$idConn = mysql_connect("$mysql_service.address.public:$mysql_service.port","$mysql_service.username","$mysql_service.password");
+						//mysql_select_db('prueba',$idConn);
+						//$query = 'SELECT * FROM prueba.estados';
+						//$res = mysql_query($query, $idConn) or die(mysql_error());
+						$get = new Connection("db1");
+						$res = $get->query("SELECT * FROM estados");
+						print "Numero de estados: ";
+						print mysql_num_rows($res);
+						print "\n\r";
+						$rowarray = $get->fetch($res);
+						print "<table>\n\r";  
+						foreach ($rowarray as $row) {
+  						 print "<tr>\n\r";  
+    					foreach ($row as $col) {
+        				print "\t<td>$col</td>\n\r";
+  						}
+  						 print "</tr>\n\r";
 						}
 					?>
 
